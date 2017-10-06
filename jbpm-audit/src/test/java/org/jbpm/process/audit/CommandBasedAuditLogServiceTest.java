c841cf3179a1587fb4c7fa7aa0a11e5ade3cf57c
dcihak <dcihak@redhat.com>
2017-10-06 16:47:58 +0200

    1. AsyncAuditLogProducerTest.testAsyncAuditLoggerCompleteWithVariablesCustomIndexer fixed when comparing process variable values loaded from audit log. Sometimes variable values were retrieved from audit log in a wrong order. 2. CommandBasedAuditLogServiceTest fixed: audit log clearing added during tearDown.

--- jbpm-audit/src/test/java/org/jbpm/process/audit/CommandBasedAuditLogServiceTest.java
+++ jbpm-audit/src/test/java/org/jbpm/process/audit/CommandBasedAuditLogServiceTest.java
@@ -69,6 +69,7 @@ public class CommandBasedAuditLogServiceTest extends AbstractAuditLogServiceTest
     public void tearDown() throws Exception {
         session.dispose();
         session = null;
+        auditLogService.clear();
         auditLogService = null;
         cleanUp(context);
     }
