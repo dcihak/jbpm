61117b4a05ac907291c4e1dd7f663bd8ef40c641
dcihak <dcihak@redhat.com>
2017-10-25 09:54:16 +0200

    logging added

--- jbpm-audit/src/test/java/org/jbpm/process/audit/jms/AsyncAuditLogProducerTest.java
+++ jbpm-audit/src/test/java/org/jbpm/process/audit/jms/AsyncAuditLogProducerTest.java
@@ -395,6 +395,11 @@ public class AsyncAuditLogProducerTest extends AbstractBaseTest {
         // wait for timer
         String endNodeName = "End";
         assertTrue( "Node '" + endNodeName + "' was not triggered on time!", tpel.waitForNodeTobeTriggered(endNodeName, 2000));
+        List<String> nodesLeft = tpel.getNodesLeft();
+        this.logger.info("List of all nodes left: ");
+        for (String node : nodesLeft) {
+            this.logger.info("NODE: " + node);
+        }
         assertEquals(12, tpel.getNodesLeft().size());
         List<NodeInstanceLog> nodeInstances = logService.findNodeInstances(processInstance.getId());
 
