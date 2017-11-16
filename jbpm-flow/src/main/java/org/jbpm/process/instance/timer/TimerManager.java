6d758e94168a01a3fd422b2f2cf7d90cca45b65b
dcihak <dcihak@redhat.com>
2017-11-16 15:24:50 +0100

    timer changed

--- jbpm-flow/src/main/java/org/jbpm/process/instance/timer/TimerManager.java
+++ jbpm-flow/src/main/java/org/jbpm/process/instance/timer/TimerManager.java
@@ -278,7 +278,6 @@ public class TimerManager {
         private static final long serialVersionUID = 6004839244692770390L;
 
         public void execute(JobContext c) {
-            logger.info("StartProcessJob.execute1");
 
             ProcessJobContext ctx = (ProcessJobContext) c;
 
@@ -324,7 +323,6 @@ public class TimerManager {
         private static final long serialVersionUID = 1039445333595469160L;
 
         public void execute(JobContext c) {
-            logger.info("StartProcessJob.execute2");
 
             StartProcessJobContext ctx = (StartProcessJobContext) c;
 
