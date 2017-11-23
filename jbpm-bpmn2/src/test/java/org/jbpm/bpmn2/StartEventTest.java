0aaad0dca5786a9fb3453c1a7aede06a7799a848
dcihak <dcihak@redhat.com>
2017-11-23 13:00:37 +0100

    one test ignored

--- jbpm-bpmn2/src/test/java/org/jbpm/bpmn2/StartEventTest.java
+++ jbpm-bpmn2/src/test/java/org/jbpm/bpmn2/StartEventTest.java
@@ -326,6 +326,7 @@ public class StartEventTest extends JbpmBpmn2TestCase {
         Assertions.assertThat(getNumberOfProcessInstances("Minimal")).isEqualTo(1);
     }
 
+    @Ignore
     @Test
     public void testMultipleStartEventsRegularStart() throws Exception {
         KieBase kbase = createKnowledgeBase("BPMN2-MultipleStartEventProcessLongInterval.bpmn2");
